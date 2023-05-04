1a13534987605e7b0076633feadbecf1c2c959cb
mimo217 <mimi.zhou217@gmail.com>
2023-05-03 21:59:56 -0500

    updated files

--- script.js
+++ script.js
@@ -10,27 +10,34 @@ function resetGameBoard () {
   ];
 }
 
-let roundInfo = `Round + ${roundNum}: ${Player1} + ${Player2}`;
-console.log (roundinfo);
+let roundInfo = `Round ${roundNum}: ${Player1} + ${Player2}`;
+console.log(roundInfo);
 
 let temp = Player1;
-
 Player1 = Player2;
 Player2 = temp;
 
-let currentPlayer = Player1;
+currentPlayer = Player1;
 
 function takeTurn(cell, row) {
- if gameBoard[cell,row] !== "") {
-    showInfo("Lo siento this box has been occupied")
-return
-}
-
-let currentPlayer = 'Player1'
-
-if "checkWinner === "" {
- console.log(showInfo)
- }else{
-console.log(showInfo)
+ if (gameBoard[cell,row] !== "") {
+    showInfo('Lo siento this box has been occupied')
+    return;
+    console.log(showInfo);
  }
 }
+
+gameBoard[cell][row] = currentPlayer;
+
+if (checkWinner()) {
+  showInfo(currentPlayer + ' wins!');
+ } else if (gameIsTied()) {
+  showInfo("It's a draw!")
+  return;
+  console.log(showInfo)
+ }
+
+
+ function newRound() {
+}
+ 
