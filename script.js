852939bdfcd93fd7d60af86df2156ae88f78a5a1
mimo217 <mimi.zhou217@gmail.com>
2023-05-12 20:21:39 -0500

    added finish touches to HTML, CSS and Javascript

--- script.js
+++ script.js
@@ -1,17 +1,14 @@
-/* ---- constants ----*/
 const roundLimit = 5;
 const player1 = 'O';
-const player2 = 'X'
-
-//Define icons per destination(game round)
-const destinations = {
-  'Paris':{ player1:'https://i.imgur.com/JwWuqqM.jpg',player2:'https://i.imgur.com/8aunzUM.jpg'},
-  'Barcelona':{ player1:'',player2:''},
-  'London':{ player1:'',player2:''},
-  'Tokyo':{ player1:'https://i.imgur.com/kFLe06Q.jpg',player2:''},
-  'Beijing':{ player1:'',player2:''},
-}
-
+const player2 = 'X';
+const dest1X = 'https://i.imgur.com/JwWuqqM.jpg';
+const dest1O = 'https://i.imgur.com/8aunzUM.jpg';
+const dest2X = 'https://i.imgur.com/qyts1DG.jpg';
+const dest2O = 'https://i.imgur.com/r2S5Hl4.jpg';
+const dest3X = 'https://i.imgur.com/mRmeJ1m.png';
+const dest3O = 'https://i.imgur.com/rjcSSP6.png';
+const dest4X = 'https://i.imgur.com/RIiikxh.jpg';
+const dest4O = 'https://i.imgur.com/ezki4a4.png';
 
 /* ---- state variables ----*/
 let gameBoard = [
@@ -21,6 +18,9 @@ let gameBoard = [
 ];
 let currentPlayer = player1;
 let gameIsOngoing = true;
+let player1ImgSrc = '';
+let player2ImgSrc = '';
+
 
 function resetGameBoard(destination) {
   //reset the game board
@@ -41,8 +41,29 @@ function resetGameBoard(destination) {
   gameIsOngoing = true;
   renderGameBoard();
 
-  // //set cell status
-  // updateCellStatus();
+  //Set img sources for game pieces
+  if (destination === 'Paris') {
+    player1ImgSrc = 'https://i.imgur.com/JwWuqqM.jpg'
+    player2ImgSrc = 'https://i.imgur.com/8aunzUM.jpg'
+  }
+  if (destination === 'Lima') {
+    player1ImgSrc = 'https://i.imgur.com/qyts1DG.jpg'
+    player2ImgSrc = 'https://i.imgur.com/r2S5Hl4.jpg'
+  }
+  if (destination === 'London') {
+    player1ImgSrc = 'https://i.imgur.com/mRmeJ1m.png'
+    player2ImgSrc = 'https://i.imgur.com/rjcSSP6.png'
+  }
+  if (destination === 'Cairo') {
+    player1ImgSrc = 'https://i.imgur.com/JJ81PPR.jpg'
+    player2ImgSrc = 'https://i.imgur.com/rjcSSP6.png'
+  }
+  if (destination === 'Beijing') {
+    player1ImgSrc = 'https://i.imgur.com/RIiikxh.jpg'
+    player2ImgSrc = 'https://i.imgur.com/ezki4a4.png'
+  }
+
+
 }
 
 /*--- event listeners ----*/
@@ -58,13 +79,13 @@ document.getElementById('dest1').addEventListener('click', function () {
   resetGameBoard('Paris');
 });
 document.getElementById('dest2').addEventListener('click', function () {
-  resetGameBoard('Barcelona');
+  resetGameBoard('Lima');
 });
 document.getElementById('dest3').addEventListener('click', function () {
   resetGameBoard('London');
 });
 document.getElementById('dest4').addEventListener('click', function () {
-  resetGameBoard('Tokyo');
+  resetGameBoard('Cairo');
 });
 document.getElementById('dest5').addEventListener('click', function () {
   resetGameBoard('Beijing');
@@ -81,28 +102,39 @@ function removeClickEvent() {
   });
 }
 
+function showMessage(message) {
+  const popupBanner = document.getElementById('popup-banner');
+  const popupMessage = document.getElementById('popup-message');
+  const closeBtn = document.getElementById('close-btn');
+
+  popupMessage.textContent = message;
+  popupBanner.style.display = 'block';
+  closeBtn.onclick = function() {
+    popupBanner.style.display = 'none';
+  }
+}
 function displayCell(row, col, cell) {
   console.log(row, col, gameBoard[row][col])
 
   if (gameBoard[row][col] === '') {
     const img = document.createElement('img');
-    img.src = currentPlayer === player1 ?  'https://i.imgur.com/JwWuqqM.jpg'  : 'https://i.imgur.com/8aunzUM.jpg'
+    img.src = currentPlayer === player1 ? player1ImgSrc : player2ImgSrc
     cell.innerHTML = '';
     cell.appendChild(img);
     gameBoard[row][col] = currentPlayer;
 
+
     const winner = checkWin();
-    //check for a winner
+    //check for a winners
     if (winner !== null) {
       gameIsOngoing = false;
-      alert(`${currentPlayer} wins!`);
       removeClickEvent();
-
+      showMessage(`Passenger ${currentPlayer === player1 ? '1' : '2'} wins!`);
       // check for a tie
     } else if (checkDraw()) {
       gameIsOngoing = false;
-      alert('The game is a draw!');
       removeClickEvent();
+      showMessage('The game is a draw!');
     } else {
       //change player
       currentPlayer = (currentPlayer == 'O') ? 'X' : 'O';
@@ -146,9 +178,10 @@ function checkWin() {
       return gameBoard[0][2];
     }
 
-  return null;
+    return null;
   }
 }
+
 
 function checkDraw() {
   //check if all cells are populated
@@ -162,4 +195,6 @@ function checkDraw() {
 
   return true;
 }
+
 renderGameBoard();
+resetGameBoard();
